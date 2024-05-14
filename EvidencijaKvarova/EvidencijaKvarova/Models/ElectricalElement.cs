bc7b5722a63059b8e418a59fc10328851012ed9b
Tomasevic Nikola <tomasevic444@gmail.com>
2024-05-14 19:15:32 +0200

    +Models

---
+++ EvidencijaKvarova/EvidencijaKvarova/Models/ElectricalElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencijaKvarova.Klase
+{
+    public class ElectricalElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string VoltageLevel { get; set; } = "srednji napon";
+    }
+}
---
+++ EvidencijaKvarova/EvidencijaKvarova/Models/Fault.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidencijaKvarova
+{
+    public class Fault
+    {
+        public string Id { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Status { get; set; } = "Nepotvrdjen";
+        public string ShortDescription { get; set; }
+        public string ElementId { get; set; }
+        public string DetailedDescription { get; set; }
+        public List<Action> Actions { get; set; } = new List<Action>();
+    }
+}
